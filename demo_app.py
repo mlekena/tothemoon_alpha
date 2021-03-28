adb3daa6d9c21db15dc76ecb300ed479f3625efc
theko <thekolekena@gmail.com>
2021-03-28 10:56:04 -0700

    initial unifiedContext implemented

--- demo_app.py
+++ demo_app.py
@@ -13,6 +13,40 @@ from streamlit import sidebar as sbar
 import itertools
 
 
+class UnifiedContext(object):
+    def __init__(self, user: str, page_cache: Dict[str, Any]) -> None:
+        self.user = user
+        self.page_cache = page_cache
+
+    def RegisterPage(self, page_id: str) -> None:
+        pass
+
+
+UNIFIED_CONTEXT_CACHE_LOCATION = "__ucc/"
+
+
+def LoadUnifiedContext() -> UnifiedContext:
+    ucc = os.path.join(UNIFIED_CONTEXT_CACHE_LOCATION, "theko.json")
+    if not os.path.exists(UNIFIED_CONTEXT_CACHE_LOCATION):
+        return UnifiedContext(user="theko", page_cache={})
+        # os.mkdir(UNIFIED_CONTEXT_CACHE_LOCATION)
+    with open(ucc, 'r') as uccfile:
+        uccjson = json.load(uccfile)
+        return UnifiedContext(user=uccjson["user"],
+                              page_cache=uccjson["page_cache"])
+
+
+def StoreUnifiedContext(ctx: UnifiedContext) -> bool:
+    if not os.path.exists(UNIFIED_CONTEXT_CACHE_LOCATION):
+        os.mkdir(UNIFIED_CONTEXT_CACHE_LOCATION)
+    ucc = os.path.join(UNIFIED_CONTEXT_CACHE_LOCATION,
+                       "{}.json".format(ctx.user))
+    with open(ucc, "w") as uccfile:
+        json.dump({"user": ctx.user, "page_cache": ctx.page_cache}, uccfile)
+        return True
+    return False
+
+
 @st.cache()  # type: ignore
 def get_data() -> pd.DataFrame:
     return pd.read_csv("__data_file/C_hist_data.csv")
@@ -92,7 +126,6 @@ class OnTriggerPresenter(object):
         self.to_trigger = call
 
     def RenderTrigger(self) -> None:
-        # (label=self.trigger_label, key=self.id)
         self.trigger_button = st.button(self.trigger_label)
         if self.trigger_button:
             print("%s flipped" % self.trigger_label)
@@ -117,7 +150,7 @@ class StockCategoryHandler(object):
 
 
 def ReadCategoryFromJsonFile(filepath: pathlib.Path) -> StockCategoryHandler:
-    with open(filepath) as json_file:
+    with open(filepath, 'r') as json_file:
         category_data = json.load(json_file)
         return StockCategoryHandler(
             id=category_data["id"],
@@ -183,9 +216,6 @@ def RenderHome() -> None:
             rtn_df[column] = pd.Series(data["Close"])
         return rtn_df
 
-    # data = get_data()
-    # time_cprice_data = data[["Close"]]  # .set_index("Date")
-    # charted_data = time_cprice_data
     chart_placeholder = st.empty()
     stocks_to_show: List[Tuple[str, bool]]
     _1, mc, _2 = st.beta_columns(3)
@@ -196,8 +226,6 @@ def RenderHome() -> None:
     st.dataframe(charted_data)
     # prints a table of the closing prices.
     chart_placeholder.line_chart(charted_data)
-
-    # pbar = st.progress(0)
 
 
 def RenderStockPageOne() -> None:
@@ -237,6 +265,8 @@ def RenderStockPageOne() -> None:
             st.info("Unable to find ticker :(")
         else:
             st.success("Found ticker.")
+
+    GoToStockAllocation = st.empty()
     RenderTickerSearcher()
 
     def RenderEditorsChoiceStockList() -> None:
@@ -249,7 +279,6 @@ def RenderStockPageOne() -> None:
 
     def RenderStockThemeCarousel() -> None:
         def GetCarouselMembers() -> List[Tuple[ImageResourceHandler, OnTriggerPresenter]]:
-            # returns List[Tuple[ThemeImage, ThemeExploreButton]]
             triggered_resource = list(
                 map(lambda i: OnTriggerPresenter("%d" %
                                                  i, RenderCategory), range(4)))
@@ -258,32 +287,6 @@ def RenderStockPageOne() -> None:
                              ImageResourceHandler("animal_3"),
                              ImageResourceHandler("animal_4")], triggered_resource))
 
-        def GetCurrentCarouselWindow(num_on_display: int) -> int:
-            # returns number between 1 and 3
-            windows = num_on_display//3
-
-            rtn: int = st.slider(
-                "Slide through our stock categories", min_value=0, max_value=windows)
-            return rtn
-
-        def RenderImgAndButton(c_members: List[Tuple[ImageResourceHandler, OnTriggerPresenter]]) -> None:
-            for resource, layer in zip(c_members, st.beta_columns(3)):
-                with layer:
-                    resource[0].Materialize()
-                    resource[1].RenderTrigger()
-
-    #     carousel_members = GetCarouselMembers()
-
-    #     c_window = GetCurrentCarouselWindow(len(carousel_members))
-
-    #     carousel_member_limit = 3
-    #     RenderImgAndButton(
-    #         carousel_members[c_window: c_window + carousel_member_limit])
-
-    #     for _, trigger in carousel_members:
-    #         trigger.RenderIfTriggered()
-
-    # RenderStockThemeCarousel()
     RenderCategory()
     gathered_stock_selection: Set[str] = set()
     for category in CATEGORIES:
@@ -291,45 +294,22 @@ def RenderStockPageOne() -> None:
             s for s in InflateCategory(category)}
     print()
     print(gathered_stock_selection)
-    # ls = list(map(lambda i: "./resources/animal_{}.jpg".format(i), range(0, 5, 1)))
-    # print(ls)
-    # img_keys = [(img, key) for img, key in zip(
-    #     map(lambda i: "./resources/animal_{}.jpg".format(i), range(0, 5, 1)), range(4))]
-    # images_to_render = []
+    GoToStockAllocation.beta_container()
+    if not gathered_stock_selection:
+        with GoToStockAllocation:
+            next = st.button("Next", key="NoStocksSelected")
+            if next:
+                st.warning(
+                    "Please find and select some stocks before allocation.")
+    else:
+        with GoToStockAllocation:
+            if st.button("Next", key="StocksSelected"):
+                # RenderStockAllocation()
+                pass
 
-    # section_button_rotators = st.beta_columns(4)
-    # n = 0
-    # with section_button_rotators[1]:
-    #     if st.button("<"):
-    #         n += 1
-    # with section_button_rotators[2]:
-    #     st.button(">")
-    # rotator = 1
-    # for _ in range(3):
-    #     images_to_render.append(img_keys[rotator])
-    #     rotator = (rotator + 1 % 4)
-    # l, m, r = st.beta_columns(3)
-    # with l:
-    #     rk1 = images_to_render[0]
-    #     st.image(rk1[0], key=rk1[1])
-    #     st.button("Explore", key="b1")
-    # with m:
-    #     rk2 = images_to_render[1]
-    #     st.image(rk2[0], key=rk2[1])
-    #     st.button("Explore", key="m")
-    # with r:
-    #     rk3 = images_to_render[2]
-    #     st.image(rk3[0], key=rk3[1])
-    #     st.button("Explore", key="t")
-
-    # st.write(n)
-# st.text()
-
-# Main Construction
-
-
+                # Main Construction
 selected_window = GenerateSideBar()
-
+ctx = LoadUnifiedContext()
 if selected_window == home_title:
     st.write("Home page")
     RenderHome()
@@ -340,3 +320,5 @@ elif selected_window == social_title:
     st.write("Social")
 else:
     st.error("Unknown section selected!")
+
+StoreUnifiedContext(ctx)
