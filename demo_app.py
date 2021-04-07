b0be3e1c1fbe9830653e0b700ea50494bae41979
theko <thekolekena@gmail.com>
2021-04-06 19:11:33 -0700

    refactored out UnifiedContext into unified_context.py

--- demo_app.py
+++ demo_app.py
@@ -21,8 +21,8 @@ from decouple import config
 from streamlit import sidebar as sbar
 from streamlit.report_thread import get_report_ctx
 
-from src.Cache import Cache, TEXT_T, FLOAT_T
-
+from src.cache import Cache, TEXT_T, FLOAT_T
+from src.unified_context import UnifiedContext, LoadUnifiedContext, StoreUnifiedContext
 # Implement this system in a MVC fashion where the pages are the views and
 # the controller is the unified context system.
 
@@ -56,38 +56,36 @@ from src.Cache import Cache, TEXT_T, FLOAT_T
 # context to which we are running such as who the current user is so this might be better
 # served as a json maybe??
 
+# class UnifiedContext(object):
 
-class UnifiedContext(object):
+#     cache_schema = [("currentPage", TEXT_T)]
 
-    cache_schema = [("currentPage", TEXT_T)]
+#     def __init__(self, user: str) -> None:
+#         self.user = user
+#         # self.page_cache = page_cache
+#         self.cache = Cache.get_instance()
+#         self.user_data: pd.DataFrame = pd.DataFrame([])
+#         self.unified_context_id = "_id_UnifiedContextCoreCache"
+#         self.cache.InitCache(self.unified_context_id,
+#                              UnifiedContext.cache_schema)
+#         self.app_state: pd.DataFrame = self.cache.read_state_df(
+#             self.unified_context_id)
+#     # String type used to get forward usage of type names
+#     # https://stackoverflow.com/questions/33533148/how-do-i-type-hint-a-method-with-the-type-of-the-enclosing-class
 
-    def __init__(self, user: str) -> None:
-        self.user = user
-        # self.page_cache = page_cache
-        self.cache = Cache.get_instance()
-        self.user_data: pd.DataFrame = pd.DataFrame([])
-        self.unified_context_id = "_id_UnifiedContextCoreCache"
-        self.cache.InitCache(self.unified_context_id,
-                             UnifiedContext.cache_schema)
-        self.app_state: pd.DataFrame = self.cache.read_state_df(
-            self.unified_context_id)
-    # String type used to get forward usage of type names
-    # https://stackoverflow.com/questions/33533148/how-do-i-type-hint-a-method-with-the-type-of-the-enclosing-class
+#     def SetCurrentPageManager(self, page_manager: "PageManager") -> None:
+#         # TODO Think about how to ensure this gets called before each render
+#         self.current_page_manager = page_manager
 
-    def SetCurrentPageManager(self, page_manager: "PageManager") -> None:
-        # TODO Think about how to ensure this gets called before each render
-        self.current_page_manager = page_manager
+#     def RestorePageState(self, page_manager_id: str) -> pd.DataFrame:
+#         print("Restore State")
+#         return {}
 
-    def RestorePageState(self, page_manager_id: str) -> pd.DataFrame:
-        print("Restore State")
-        return {}
+#     def StorePageState(self, state: pd.DataFrame, page_manager_id: str) -> None:
+#         print("Store State")
 
-    def StorePageState(self, state: pd.DataFrame, page_manager_id: str) -> None:
-        print("Store State")
-
-    def CheckCacheExists(self, page_manager_id: str) -> bool:
-        pass
-
+#     def CheckCacheExists(self, page_manager_id: str) -> bool:
+#         pass
 
 class Page(object):
 
@@ -147,22 +145,22 @@ class PageManager(object):
         raise NotImplementedError
 
 
-UNIFIED_CONTEXT_CACHE_LOCATION = "__ucc/"
+# UNIFIED_CONTEXT_CACHE_LOCATION = "__ucc/"
 
 
-def LoadUnifiedContext() -> UnifiedContext:
-    return UnifiedContext(user="Theko")
-    # ucc = os.path.join(UNIFIED_CONTEXT_CACHE_LOCATION, "theko.json")
-    # if not os.path.exists(UNIFIED_CONTEXT_CACHE_LOCATION):
-    #     return UnifiedContext(user="theko")
-    #     # os.mkdir(UNIFIED_CONTEXT_CACHE_LOCATION)
-    # with open(ucc, 'r') as uccfile:
-    #     uccjson = json.load(uccfile)
-    #     return UnifiedContext(user="saved_user")
+# def LoadUnifiedContext() -> UnifiedContext:
+#     return UnifiedContext(user="Theko")
+#     # ucc = os.path.join(UNIFIED_CONTEXT_CACHE_LOCATION, "theko.json")
+#     # if not os.path.exists(UNIFIED_CONTEXT_CACHE_LOCATION):
+#     #     return UnifiedContext(user="theko")
+#     #     # os.mkdir(UNIFIED_CONTEXT_CACHE_LOCATION)
+#     # with open(ucc, 'r') as uccfile:
+#     #     uccjson = json.load(uccfile)
+#     #     return UnifiedContext(user="saved_user")
 
 
-def StoreUnifiedContext(ctx: UnifiedContext) -> bool:
-    pass
+# def StoreUnifiedContext(ctx: UnifiedContext) -> bool:
+#     pass
 
 
 @st.cache()  # type: ignore
