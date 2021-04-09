ba8779fc8f1f25f370eebcf3a4d1e9b609672b84
theko <thekolekena@gmail.com>
2021-04-08 20:22:51 -0700

    begin unittesting cache system

--- src/cache.py
+++ src/cache.py
@@ -6,7 +6,7 @@ import pandas as pd
 import psycopg2
 from decouple import config
 from functools import reduce
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, MetaData
 from typing import Dict, List, Tuple, Any, Optional, Callable, Set, NewType
 
 
@@ -18,43 +18,47 @@ PGUSER = config("DEVPOSTGRESUSER")
 PGPASS = config("DEVPOSTGRESPASSWORD")
 
 
-class Cache(object):
+class Cache:
     """ designed to be a lazy evaluate"""
     __instance: "Cache" = None  # type: ignore
-    __db_engine = create_engine(
+    engine = create_engine(
         'postgresql://%s:%s@localhost:5432/postgres' % (PGUSER, PGPASS))
+    metadata = MetaData()
 
-    def __init__(self) -> None:
+    def __init__(self, connection_string: str) -> None:
         print("Calling initi Cache babe")
         if Cache.__instance == None:
             Cache.__instance = self
+            self.engine = create_engine(connection_string)
+            self.metadata.create_all(self.engine)
+            self.connection = self.engine.connect()
         else:
             raise RuntimeError(
                 "Attempting to create multiple Cache objects. Use get_instance(..) function.")
 
     @staticmethod
-    def get_instance() -> "Cache":
+    def get_instance(connection_string: str) -> "Cache":
         if not Cache.__instance:
-            Cache()
+            Cache(connection_string)
         return Cache.__instance
 
     def read_state_df(self, session_id: str) -> pd.DataFrame:
         try:
-            df = pd.read_sql_table(session_id, con=Cache.__db_engine)
+            df = pd.read_sql_table(session_id, con=self.engine)
         except:
             df = pd.DataFrame([])
         return df
 
     def write_state_df(self, df: pd.DataFrame, session_id: str) -> None:
-        df.to_sql('%s' % (session_id), Cache.__db_engine, index=False,
+        df.to_sql('%s' % (session_id), self.engine, index=False,
                   if_exists='replace', chunksize=1000)
 
-    def write_state(column, value, session_id) -> None:  # type: ignore
-        Cache.__db_engine.execute("UPDATE %s SET %s='%s'" %
-                                  (session_id, column, value))
+    def write_state(self, column, value, session_id) -> None:  # type: ignore
+        self.engine.execute("UPDATE %s SET %s='%s'" %
+                            (session_id, column, value))
 
-    def read_state(column, session_id) -> Any:  # type: ignore
-        state_var = Cache.__db_engine.execute(
+    def read_state(self, column, session_id) -> Any:  # type: ignore
+        state_var = self.engine.execute(
             "SELECT %s FROM %s" % (column, session_id))
         state_var = state_var.first()[0]
         return state_var
---
+++ src/cache_test.py
@@ -0,0 +1,31 @@
+from . cache import Cache
+
+import pandas as pd
+import pytest
+import pytest_benchmark
+from sqlalchemy import (MetaData, Table, Column, Integer, Numeric, String,
+                        DateTime, ForeignKey, Boolean, create_engine)
+
+
+class TestCache:
+    cache = Cache.get_instance("sqlite:///:memory:")
+    sp_session_id = "cache_test_id"
+    sp_table = Table(sp_session_id,
+                     cache.metadata,
+                     Column("ticker", String(255), primary_key=True),
+                     Column("allocation", Numeric(3, 12)))
+    insert = sp_table.insert()
+
+    def prep_fake_stock_picker_data(self) -> None:
+        mock_data = pd.DataFrame({'ticker': [], "allocation": []})
+        pass
+
+    def test_stock_picker_data_flow(self) -> None:
+        # self.prep_fake_stock_picker_data()
+        data = pd.DataFrame({"ticker": ['AAPL', "TSLA", "COIN"],
+                             "allocation": [0, 0, 0]})
+        TestCache.cache.write_state_df(data, TestCache.sp_session_id)
+        assert data.shape == TestCache.cache.read_state_df(
+            TestCache.sp_session_id).shape
+        assert all(actual == expected for actual, expected in zip(data.columns, TestCache.cache.read_state_df(
+            TestCache.sp_session_id).columns))
